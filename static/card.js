9fb9976b697db62dbb72de35944297092ccdc685
abd-08-yogeshwaran <yogeshwarangsm1@gmail.com>
2023-06-22 22:49:11 +0530

    22.06.2023-1

--- static/card.js
+++ static/card.js
@@ -1,10 +1,21 @@
-var p = [["ABD", 20, "Hiroshima"],["CDQ", 20, "Subara"],["VKA", 20, "Tokyo"],["ABD", 20, "Hiroshima"],["CDQ", 20, "Subara"],["VKA", 20, "Tokyo"]];
+var lorem = `
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+	when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	It has survived not only five centuries, but also the leap into electronic typesetting,
+	remaining essentially unchanged.
+	It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+	and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+`
+var p = [["ABD", lorem, "Hiroshima"],["CDQ", lorem, "Subara"],["VKA", lorem, "Tokyo"]];
+
 for(let i=0; i<p.length; i++){
 	let card = ` 
+	<a href="#" class="crd">
 	<div class="col">
-		<div class="card h-100 crd">
+		<div class="card h-100 crd1">
 		    <div class="card-body">
-		        <h5 class="card-title"> ${p[i][0]} </h5>
+		        <h4 class="card-title"> ${p[i][0]} </h4>
 		        <p class="card-text">
 		          ${p[i][1]}
 		        </p>
@@ -12,19 +23,11 @@ for(let i=0; i<p.length; i++){
 		      <div class="card-footer foot">
 		        <small class="text-muted">
 		        	<p>${p[i][2]}</p>
-		        	<form method="POST">
-				    		<center>
-							  	<a href="p2.html">
-							  		<button class="btn btn-primary" onclick=cardDetails >
-							  			Buy
-							  		</button>
-							  	</a>
-				    		</center>
-				    </form>
 		        </small>
 		      </div>
 		    </div>
 		  </div>
+	</a>
 `
 	document.write(card);
 	document.write("<br><br>");
